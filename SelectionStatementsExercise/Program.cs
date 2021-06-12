c0faca3129311ae8b1b0ee267472d96b8679f867
Danielle Smith <smith.ddms@gmail.com>
2021-06-12 16:47:53 -0500

    Updated and Fixed Bugs.

--- SelectionStatementsExercise/Program.cs
+++ SelectionStatementsExercise/Program.cs
@@ -7,59 +7,63 @@ namespace SelectionStatementsExercise
         static void Main(string[] args)
         {
             /* My favorite number */
-            /* Need to figure out how to get help response to work */
-            /* Need to figure out how to stop the repeated correct answer response if user guesses number correct on the first try*/
 
             Console.WriteLine("Try to guess my favorite number.");
             Console.WriteLine("Enter your first guess to begin.");
 
-            int guess = int.Parse(Console.ReadLine());
+            string guess = Console.ReadLine();
             var myNum = 8;
             string statement = "help";
 
             do
             {
-                if (Convert.ToString(guess) == statement)
+                if (guess == statement)
                 {
                     Console.WriteLine("Lower than 50, but higher than 5.");
                 }
-                else if (guess >= 1000)
+                else if (Convert.ToInt32(guess) >= 1000)
                 {
                     Console.WriteLine("Way too high!!");
                     Console.WriteLine("Consider typing 'help' to get a hint.");
                 }
-                else if (guess >= 13)
+                else if (Convert.ToInt32(guess) >= 13)
                 {
                     Console.WriteLine("Try reducing your number!");
                 }
-                else if (guess <= 12 && guess >= 9)
+                else if (Convert.ToInt32(guess) <= 12 && Convert.ToInt32(guess) >= 9)
                 {
                     Console.WriteLine("You are really close! A little lower.");
                 }
-                else if (guess <= 7 && guess >= 4)
+                else if (Convert.ToInt32(guess) <= 7 && Convert.ToInt32(guess) >= 4)
                 {
                     Console.WriteLine("You are really close! A little higher.");
                 }
-                else if (guess <= 3)
+                else if (Convert.ToInt32(guess) <= 3)
                 {
                     Console.WriteLine("Too low!");
                 }
                 else
-                { 
+                {
                     Console.WriteLine("Excellent! You guessed my favorite number!!");
                     break;
                 }
 
-                Console.WriteLine("Enter your next guess again:");
-                guess = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter your next guess:");
+                guess = Console.ReadLine();
+
+                switch (guess)
+                {
+                    case "help":
+                        break;
+                    case "8":
+                        Console.WriteLine("Excellent! You guessed my favorite number!!");
+                        break;
+                }
             }
-            while (Convert.ToInt32(guess) != 8);
-            Console.WriteLine("Excellent! You guessed my favorite number!!");
-            
+            while (guess == statement || Convert.ToInt32(guess) != myNum);
 
 
-
-            /* Computer generated random number picker */
+            /* Computer generated random number picker 
 
             var num = new Random();
             var systemNumber = num.Next(1, 50);
@@ -83,8 +87,7 @@ namespace SelectionStatementsExercise
                     Console.WriteLine("Your number is correct!!");
                 }
             }
-            while (userInput != systemNumber);
-
+            while (userInput != systemNumber);*/
 
 
 
